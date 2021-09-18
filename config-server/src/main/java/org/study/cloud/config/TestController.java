b93a9e9fdc5632ac90dfcad702b8be029ca54f3f
lipo <lipo@qq.com>
2021-09-18 11:10:43 +0800

    log

--- config-server/src/main/java/org/study/cloud/config/TestController.java
+++ config-server/src/main/java/org/study/cloud/config/TestController.java
@@ -1,14 +1,17 @@
 package org.study.cloud.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
     //http://localhost:9050/test/hello
     @RequestMapping("/hello")
     public Object hello() {
+        log.info("hello");
         return "hello";
     }
 }
