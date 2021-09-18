154b1ae7216b72a9f9e86a3c4cf95570e6a10314
lipo <lipo@qq.com>
2021-09-18 14:52:33 +0800

    sdf

--- eureka/src/main/java/org/study/cloud/eureka/TestController.java
+++ eureka/src/main/java/org/study/cloud/eureka/TestController.java
@@ -4,14 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
     //http://localhost:9050/test/hello
     @RequestMapping("/hello")
-    public Object hello() {
-        log.info("hello");
+    public Object hello(HttpServletRequest request) {
+        log.info(request.getRequestURL() + "-hello");
         return "hello";
     }
 }
